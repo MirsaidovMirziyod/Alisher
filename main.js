70b25af84b4987212744007f675f0a761838c87e
Mirziyod <mirsaidovmirziyod1507@gmail.com>
2023-05-09 22:19:03 +0500

    features

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-let elResult = document.querySelector('h1')
+let elResult = document.querySelector("h1");
 
 let money = +prompt("Son kiriting!");
 let plainCost = 500;
 let hotelCost = 250;
-let enternaimentCost = 120;
+let funCost = 120;
 
-plainCost = 500 * 11000.34;
-hotelCost = 250 * 11000.34;
-enternaimentCost = 120 * 12354.34;
+let dollar = 11000.34;
+let euro = 12354.34;
 
-let all = plainCost + plainCost + enternaimentCost;
+plainCost = plainCost * dollar;
+hotelCost = hotelCost * dollar;
+funCost = funCost * euro;
+
+let all = plainCost + plainCost + funCost;
+let notEnough = all - money;
 
 if (money - all > 0) {
   console.log("Oq yo'l Alisher");
-  elResult.textContent = "Oq yo'l Alisher"
+  elResult.textContent = "Oq yo'l Alisher";
 } else {
   console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan");
-  elResult.textContent = "Alisher, ozgina sabr qilish kerak bo'lar ekan"
+  console.log("sanga " + Math.round(notEnough) + " so'm yetmayabdi");
+  elResult.textContent = "Alisher, ozgina sabr qilish kerak bo'lar ekan";
 }
